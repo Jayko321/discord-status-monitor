0565ae36d3536e76bdea71bd2c1f9ba69eb26003
Jayko321 <62258757+Jayko321@users.noreply.github.com>
2025-03-24 08:44:35 +0300

    Safe maths on integer types

--- src/commands/execute.rs
+++ src/commands/execute.rs
@@ -1,61 +1,15 @@
+use std::sync::Mutex;
+
 use crate::discord_script::interpreter::Interpreter;
 use crate::discord_script::parser::Parser;
 use crate::discord_script::tokenizer::*;
 use serenity::builder::{CreateCommand, CreateCommandOption};
 use serenity::model::application::{CommandOptionType, ResolvedOption, ResolvedValue};
 
-#[allow(dead_code)]
-struct EventScriptRuntime; //pub fn event(event: Event) -> Result<(), RuntimeError>
-#[allow(dead_code)]
-struct Runtime; // pub fn execute() -> Result<i64, RuntimeError>
-
-//file.ds event script
-//when(msg = message("cool")) { reply(msg, "response")}
-//1. Read a file
-//2. Try to find first token
-//3. Convert a token to a keyword
-//4. Proccess a keyword to ensure that it is correct
-//5. Convert keyword to instruction
-//6. goto 2
-
-//Script
-//when(msg = message("smart message")) { reply(msg, "smart_reply") }
-//Global Script or a Script file
-//pub function is_cool_name(name: string) -> bool {
-//   return true;
-//}
-// //utils.ds if Script file
-//Usage in a script
-//import utils.ds //only if Script file
-//when(message(match(""))) {
-// if utils::is_cool_name() {
-//   reply("Wow, what a cool name")
-// }
-//}
-
 //Ideas:
 //1) 2 versions of a script 1 that executes on an event(EventScript) and 1 that executes on
 //   demand(Script)
 //That means 2 runtimes one that is embedded into a discord-bot and one that is created on demand
-//
-
-//
-//
-
-//let token = Tokenizer.next_token();
-//let keyword = from_token(token)
-//if keyword.requires_token() {
-// recurse until no tokens required
-//}
-//return keyword
-
-//#[allow(dead_code)]
-//enum KeyWordSpec {
-////What keyword relies on
-//When(Action),
-//If(Condition),
-//ElseIf(Condition),
-//}
 
 pub fn run(options: &[ResolvedOption]) -> String {
     let mut res_string = String::new();
@@ -65,31 +19,36 @@ pub fn run(options: &[ResolvedOption]) -> String {
         ..
     }) = options.first()
     {
-        res_string = format!("Script file contents: {}", file);
+        //res_string = format!("Script file contents: {}", file);
         script = file.to_string();
     }
 
+    let script_output = Mutex::new(res_string.clone());
     if !script.is_empty() {
         let tokens = Lexer::tokenize(script).unwrap();
-        let token_str = tokens.iter().map(|x| format!("{x}")).collect::<Vec<String>>().join("\n");
+        //let token_str = tokens
+        //    .iter()
+        //    .map(|x| format!("{x}"))
+        //    .collect::<Vec<String>>()
+        //    .join("\n");
+        //res_string = format!("Tokens: {token_str}");
+
         let ast = Parser::parse(tokens);
-        res_string = format!("Tokens: {token_str}");
-        res_string += format!("\n{ast:#?}").as_str();
+        //res_string += format!("\n{ast:#?}").as_str();
         let mut inter = Interpreter::new();
-        inter.null_expression_out = Some(Box::new(move |value| {
+        inter.null_expression_out = Some(Box::new(|value| {
+            let mut val = script_output.lock().unwrap();
+            *val += format!("\n {:?}", value).as_str();
             println!("\n {:?}", value);
         }));
+        inter.on_error = Some(Box::new(|err| {
+            let mut val = script_output.lock().unwrap();
+            *val += format!("\n Error occured while evaluating an expression! {}", err).as_str();
+        }));
         _ = inter.execute(*ast.unwrap());
-
-
-        //println!("{ast:?}");
-        //if let Ok(tokens) = tokens {
-            //let token_str = tokens.iter().map(|x| format!("{x}")).collect::<Vec<String>>().join("\n");
-            //res_string = format!("Tokens: {token_str}");
-        //} else {
-            //res_string = format!("Error: {:?}", tokens);
-        //}
     }
+    let val = script_output.lock().unwrap();
+    res_string += val.as_str();
 
     return res_string;
 }
--- src/discord_script/interpreter.rs
+++ src/discord_script/interpreter.rs
@@ -21,10 +21,11 @@ impl Ord for Variable {
     }
 }
 
-pub struct Interpreter {
+pub struct Interpreter<'a> {
     pub vars: std::collections::HashMap<String, Vec<Variable>>,
     pub current_depth: usize,
-    pub null_expression_out: Option<Box<dyn FnMut(AbstractValue)>>,
+    pub null_expression_out: Option<Box<dyn FnMut(AbstractValue) + 'a>>,
+    pub on_error: Option<Box<dyn Fn(String) + 'a>>,
 }
 
 #[derive(Debug)]
@@ -41,12 +42,13 @@ impl Display for InterpreterErrors {
 }
 impl Error for InterpreterErrors {}
 
-impl Interpreter {
+impl Interpreter<'_> {
     pub fn new() -> Self {
         Self {
             vars: HashMap::new(),
             current_depth: 0,
             null_expression_out: None,
+            on_error: None,
         }
     }
 
@@ -54,7 +56,12 @@ impl Interpreter {
         let d = ast.get_description();
         if let AbstractStatementDescription::Block(statements) = d {
             for statement in statements {
-                _ = self.execute_statement(*statement);
+                let res = self.execute_statement(*statement);
+                if let Some(e) = res.err() {
+                    if let Some(cb) = &self.on_error {
+                        (cb)(e.to_string())
+                    }
+                }
             }
         };
         //self.execute_statement(ast.get_description());
@@ -63,7 +70,7 @@ impl Interpreter {
     pub fn execute_statement(
         &mut self,
         description: AbstractStatementDescription,
-    ) -> Result<Option<AbstractValue>, InterpreterErrors> {
+    ) -> Result<Option<AbstractValue>, Box<dyn Error>> {
         match description {
             AbstractStatementDescription::Variable(name, _size, value) => {
                 let var = Variable {
@@ -72,7 +79,7 @@ impl Interpreter {
                 };
                 if let Some(vars) = self.vars.get_mut(&name) {
                     if vars.contains(&var) {
-                        return Err(InterpreterErrors::VariableAlreadyExists(name));
+                        return Err(Box::new(InterpreterErrors::VariableAlreadyExists(name)));
                     } else {
                         vars.push(var);
                     }
@@ -102,7 +109,7 @@ impl Interpreter {
     pub fn execute_expression(
         &mut self,
         expr: AbstractExpressionDescription,
-    ) -> Result<AbstractValue, InterpreterErrors> {
+    ) -> Result<AbstractValue, Box<dyn Error>> {
         use AbstractExpressionDescription::*;
         match expr {
             Integer(value) | UnsingedInteger(value) | Float(value) | LiteralString(value) => {
@@ -114,10 +121,10 @@ impl Interpreter {
 
                 if !left_value._type.supports(binary_operations.clone()) {
                     //TODO: Custom types
-                    return Err(InterpreterErrors::Unimplemented);
+                    return Err(Box::new(InterpreterErrors::Unimplemented));
                 }
                 if !right_value._type.supports(binary_operations.clone()) {
-                    return Err(InterpreterErrors::Unimplemented);
+                    return Err(Box::new(InterpreterErrors::Unimplemented));
                 }
                 let _type = left_value._type.clone();
                 fn perform_operation<
@@ -125,49 +132,62 @@ impl Interpreter {
                         + std::ops::Add<Output = T>
                         + std::ops::Sub<Output = T>
                         + std::ops::Div<Output = T>
-                        + std::ops::Mul<Output = T>,
+                        + std::ops::Mul<Output = T>
+                        + num_traits::ops::checked::CheckedDiv
+                        + num_traits::ops::checked::CheckedSub
+                        + num_traits::ops::checked::CheckedAdd
+                        + num_traits::ops::checked::CheckedMul,
                 >(
                     left: AbstractValue,
                     right: AbstractValue,
                     op: BinaryOperations,
-                ) -> T {
+                ) -> Result<T, Box<dyn Error>> {
                     match op {
-                        BinaryOperations::Add => Into::<T>::into(left) + Into::<T>::into(right),
-                        BinaryOperations::Subtract => {
-                            Into::<T>::into(left) - Into::<T>::into(right)
-                        }
-                        BinaryOperations::Divide => Into::<T>::into(left) / Into::<T>::into(right),
-                        BinaryOperations::Multiply => {
-                            Into::<T>::into(left) * Into::<T>::into(right)
-                        }
+                        BinaryOperations::Add => Ok(Into::<T>::into(left)
+                            .checked_add(&Into::<T>::into(right))
+                            .ok_or("Overflow error")?),
+                        BinaryOperations::Subtract => Ok(Into::<T>::into(left)
+                            .checked_sub(&Into::<T>::into(right))
+                            .ok_or("Underflow error")?),
+                        BinaryOperations::Divide => Ok(Into::<T>::into(left)
+                            .checked_div(&Into::<T>::into(right))
+                            .ok_or("Zero division error! ")?),
+                        BinaryOperations::Multiply => Ok(Into::<T>::into(left)
+                            .checked_mul(&Into::<T>::into(right))
+                            .ok_or("Overflow error")?),
                     }
                 }
 
                 let res: Vec<u8> = match left_value._type.clone() {
                     Types::Integer => {
-                        perform_operation::<i64>(left_value, right_value, binary_operations)
+                        perform_operation::<i64>(left_value, right_value, binary_operations)?
                             .to_be_bytes()
                             .to_vec()
                     }
                     Types::UnsingedInteger => {
-                        perform_operation::<u64>(left_value, right_value, binary_operations)
+                        perform_operation::<u64>(left_value, right_value, binary_operations)?
                             .to_be_bytes()
                             .to_vec()
-                    },
+                    }
                     Types::Float => {
-                        perform_operation::<f64>(left_value, right_value, binary_operations)
-                            .to_be_bytes()
-                            .to_vec()
+                        let left = Into::<f64>::into(left_value);
+                        let right = Into::<f64>::into(right_value);
+                        let res = match binary_operations {
+                            BinaryOperations::Add => left + right,
+                            BinaryOperations::Subtract => left - right,
+                            BinaryOperations::Divide => left / right,
+                            BinaryOperations::Multiply => left * right,
+                        };
+                        res.to_be_bytes().to_vec()
+                    }
+                    Types::String => match binary_operations {
+                        BinaryOperations::Add => (String::from(left_value)
+                            + String::from(right_value).as_str())
+                        .into_bytes(),
+                        _ => return Err(Box::new(InterpreterErrors::Unimplemented)),
                     },
-                    Types::String => {
-                        match binary_operations {
-                            BinaryOperations::Add => (String::from(left_value) + String::from(right_value).as_str()).into_bytes(),
-                            _ => return Err(InterpreterErrors::Unimplemented)
-                        }
-
-                    },
-                    Types::Boolean => return Err(InterpreterErrors::Unimplemented), // TODO: add support for boolean operations,
-                    _ => return Err(InterpreterErrors::Unimplemented),
+                    Types::Boolean => return Err(Box::new(InterpreterErrors::Unimplemented)), // TODO: add support for boolean operations,
+                    _ => return Err(Box::new(InterpreterErrors::Unimplemented)),
                 };
 
                 Ok(AbstractValue::new(Box::new(res), _type))
